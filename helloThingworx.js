7c4fc5b0da1cd47f89ba85bcff4376357b0ee5bb
dmcclelland <dmcclelland@ptc.com>
2018-03-09 16:12:58 -0500

    functional: adds a new thing to running instance. Must be colocated with MASHUP dir of Thingworx instance on specific machine and port to work.

--- helloThingworx.js
+++ helloThingworx.js
@@ -1,1 +1,24 @@
-console.log("Hello Thingworx");
+
+
+var createThing = function() {
+    var thingName = document.getElementById('thingNameText').value;
+    console.log("thingName: " + thingName);
+    var data = null;
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("POST", "http://dmcclelland0ml.local:8080/Thingworx/Resources/EntityServices/Services/CreateThing?name=" +
+    thingName + "&thingTemplateName=GenericThing&Content-Type=application/json&Accept-Charset=UTF-8");
+    xhr.setRequestHeader("AppKey", "10000000-0000-0000-0000-000000000001");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Cache-Control", "no-cache");
+
+    xhr.send(data);
+};
